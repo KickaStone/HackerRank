afb7c169016e737a5e997e781e5addf2e9111599
Juncheng Ji <jijuncheng@sjtu.edu.cn>
2024-02-09 10:11:04 +0800

    Viral Advertising solved

---
+++ strange-advertising.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    res = [2]
+    for i in range(n-1):
+        res.append(res[-1]*3//2)
+    return sum(res)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
