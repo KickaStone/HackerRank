31fb673aec4da620c580df21962beeef35124416
Juncheng Ji <jijuncheng@sjtu.edu.cn>
2024-02-09 09:05:09 +0800

    Picking Numbers solution updated

--- picking-numbers.py
+++ picking-numbers.py
@@ -14,19 +14,32 @@ import sys
 # The function accepts INTEGER_ARRAY a as parameter.
 #
 
-def pickingNumbers(a):
-    a = sorted(a)
-    i, j = 0, 0
-    res = 1
-    while j < len(a):
-        while j < len(a) and a[j] - a[i] <= 1:
-            j += 1
-        res = max(j - i, res)
-        i += 1
-        while i < j and a[i] - a[i - 1] == 0:
-            i += 1
 
+# O(nlog n) time;O(1) space
+# def pickingNumbers(a):
+#     a = sorted(a)
+#     i, j = 0, 0
+#     res = 1
+#     while j < len(a):
+#         while j < len(a) and a[j] - a[i] <= 1:
+#             j += 1
+#         res = max(j - i, res)
+#         i += 1
+#         while i < j and a[i] - a[i - 1] == 0:
+#             i += 1
+#
+#     return res
+
+# dict O(n) time; O(n) space
+from collections import Counter
+def pickingNumbers(a):
+    cnt = Counter(a)
+    res = 0
+    for x in cnt.keys():
+        res = max(res, cnt[x-1]+cnt[x], cnt[x]+cnt[x+1])
     return res
+
+
 
 
 if __name__ == '__main__':
