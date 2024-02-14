a33cd03f0c6f764e79c987d4249e370b08c4e49c
KickaStone <35265233+KickaStone@users.noreply.github.com>
2024-02-14 11:04:07 +0800

    Find Digits solved

---
+++ Algorithms/find-digits.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    cnt = 0
+    for c in str(n):
+        cnt += 1 if int(c) != 0 and n % int(c) == 0 else 0
+    return cnt
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
