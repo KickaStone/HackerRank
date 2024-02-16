d8a8bafae4b89538909614266eb8ab7d5b924431
KickaStone <35265233+KickaStone@users.noreply.github.com>
2024-02-16 08:47:49 +0800

    Equalize the Array solved

---
+++ Algorithms/equality-in-a-array.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+from collections import Counter
+def equalizeArray(arr):
+    return len(arr) - Counter(arr).most_common(1)[0][1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
