d6281d593f8d9add7926d8f8c9dcb7d8dae484ec
KickaStone <35265233+KickaStone@users.noreply.github.com>
2024-02-18 14:22:03 +0800

    Maximum Element solved

---
+++ DataStructures/maximum-element.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'getMax' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts STRING_ARRAY operations as parameter.
+#
+
+def getMax(operations):
+    stack = [1]
+    res = []
+    for op in operations:
+        if op[0] == '1':
+            stack.append(max(int(op[2:]), stack[-1]))
+        elif op[0] == '2':
+            stack.pop()
+        else:
+            res.append(stack[-1])
+    return res
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ops = []
+
+    for _ in range(n):
+        ops_item = input()
+        ops.append(ops_item)
+
+    res = getMax(ops)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
