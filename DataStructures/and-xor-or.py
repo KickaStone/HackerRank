ef61c5785b22e8b9e05c746ad7d41f0527a780f5
Juncheng Ji <jijuncheng@sjtu.edu.cn>
2024-03-18 13:35:06 +0800

    Revert "Merge branch 'main' of https://github.com/KickaStone/HackerRank"
    This reverts commit a3ff4ddc0e59d138a70bcaf48b97905a03f77351, reversing
    changes made to 041517ccd62b26079c8defc7b577f1f0df394056.

--- DataStructures/and-xor-or.py
+++ DataStructures/and-xor-or.py
@@ -12,37 +12,22 @@ import sys
 # The function is expected to return an INTEGER.
 # The function accepts INTEGER_ARRAY a as parameter.
 #
-# def cal(x, y):
-#     return ((x & y) ^ (x | y)) & (x ^ y)  # this is equivalent to x xor y.
-# def andXorOr(a):
-#     # Write your code here
-#     stack = []
-#     res = 0
-#     for x in a:
-#         while len(stack) and stack[-1] > x:
-#             t = stack.pop()
-#             res = max(cal(x, t), res)
-#             if len(stack):
-#                 res = max(res, cal(stack[-1], t))
-#         stack.append(x)
-#
-#     while len(stack) >= 2:
-#         res = max(res, cal(stack.pop(), stack[-1]))
-#     return res
-
-# reference : https://www.hackerrank.com/rest/contests/master/challenges/and-xor-or/hackers/mosquis/download_solution
+def cal(x, y):
+    return ((x & y) ^ (x | y)) & (x ^ y)
 def andXorOr(a):
-    stack = [a[0]]
+    # Write your code here
+    stack = []
     res = 0
-    for i in range(1, len(a)):
-        y = a[i]
-        while len(stack):
-            x = stack.pop()
-            res = max(res, x^y)
-            if x < y:
-                stack.append(x)
-                break
-        stack.append(y)
+    for x in a:
+        while len(stack) and stack[-1] > x:
+            t = stack.pop()
+            res = max(cal(x, t), res)
+            if len(stack):
+                res = max(res, cal(stack[-1], t))
+        stack.append(x)
+
+    while len(stack) >= 2:
+        res = max(res, cal(stack.pop(), stack[-1]))
     return res
 
 if __name__ == '__main__':
